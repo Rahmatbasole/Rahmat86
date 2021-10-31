34052259531c67bddc6b1a9a64aab3333457ac2d
Rahmatbasole <93481338+Rahmatbasole@users.noreply.github.com>
2021-10-31 23:28:06 +0800

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,13 @@
+<?php
+
+$useragent = "xxxx";
+
+$cookie_dog = "xxxx";
+
+$cookie_ltc = "xxxx";
+
+//recommend 0.00001000
+$min_wd_dog = "0.00001000";
+
+//recommend 0.00000070
+$min_wd_ltc = "0.00000070";
